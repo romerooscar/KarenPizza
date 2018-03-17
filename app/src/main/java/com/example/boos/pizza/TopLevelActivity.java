1c37dd7a5feae787bd2960063298d9befafd12b2
Oscar Steven Romero Beron <oscar.romero@correounivalle.edu.co>
2018-03-17 18:22:51 -0500

    Fourth Commit

--- app/src/main/java/com/example/boos/pizza/TopLevelActivity.java
+++ app/src/main/java/com/example/boos/pizza/TopLevelActivity.java
@@ -1,13 +1,18 @@
 package com.example.boos.pizza;
 
 import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.ShareActionProvider;
 
 public class TopLevelActivity extends AppCompatActivity {
+
+    private ShareActionProvider mShareActionProvider;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +21,9 @@ public class TopLevelActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.barraTitle);
         setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.app_name);
     }
+
 
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()){
@@ -32,6 +39,18 @@ public class TopLevelActivity extends AppCompatActivity {
 
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_main,menu);
+
+        MenuItem item = menu.findItem(R.id.actionShare);
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        setShareIntent("¿Quieres tomar un Capuccino?");
         return super.onCreateOptionsMenu(menu);
     }
+
+    private void setShareIntent(String message) {
+        Intent intentShare = new Intent(Intent.ACTION_SEND);
+        intentShare.setType("text/plain");
+        intentShare.putExtra(Intent.EXTRA_TEXT, message);
+        mShareActionProvider.setShareIntent(intentShare);
+    }
+
 }
